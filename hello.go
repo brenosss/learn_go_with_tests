2ad7b6708fab53ac07b88454aeff3448b25038c5
Breno Silva <brenoe133@gmail.com>
2020-01-08 13:37:56 -0300

    Refactoring hello func to use switch case

--- hello.go
+++ hello.go
@@ -5,18 +5,21 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
+const spanish = "spanish"
+const french = "french"
 
 func Hello(name string, language string) string {
     if name == "" {
         name = "World"
     }
-    if language == "spanish" {
-        return spanishHelloPrefix + name
+    switch language{
+        case spanish:
+            return spanishHelloPrefix + name
+        case french:
+            return frenchHelloPrefix + name
+        default:
+            return englishHelloPrefix + name
     }
-    if language == "french" {
-        return frenchHelloPrefix + name
-    }
-    return englishHelloPrefix + name
 }
 
 func main() {
