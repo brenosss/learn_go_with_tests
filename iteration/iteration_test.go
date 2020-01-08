6b4c4ec3dcdf8bedee9b7d7b8db07872cd0c924b
Breno Silva <brenoe133@gmail.com>
2020-01-08 14:13:45 -0300

    Create a repeat argument to Repeat func
    Create a param to define how many times the character will be repeat in
    the Repeat func

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,16 +3,24 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-    repeated := Repeat("a")
-    expected := "aaaaa"
-
-    if repeated != expected {
-        t.Errorf("expected %q but got %q", expected, repeated)
-    }
+    t.Run("Repeat character a five times", func(t *testing.T) {
+        repeated := Repeat("a", 5)
+        expected := "aaaaa"
+        if repeated != expected {
+            t.Errorf("expected %q but got %q", expected, repeated)
+        }
+    })
+    t.Run("Repeat character a n times", func(t *testing.T) {
+        repeated := Repeat("a", 10)
+        expected := "aaaaaaaaaa"
+        if repeated != expected {
+            t.Errorf("expected %q but got %q", expected, repeated)
+        }
+    })
 }
 
 func BenchmarkRepeat(b *testing.B) {
     for i := 0; i < b.N; i++ {
-        Repeat("a")
+        Repeat("a", 5)
     }
 }
