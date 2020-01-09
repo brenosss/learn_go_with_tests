a40df4edd292f9af62c69add374a0f8d52f6f75b
Breno Silva <brenoe133@gmail.com>
2020-01-09 10:01:05 -0300

    Create a func to sum all numbers in an array

--- arrays_and_slices/arrays_and_slices.go
+++ arrays_and_slices/arrays_and_slices.go
@@ -7,3 +7,11 @@ func Sum(numbers []int) int {
     }
     return result
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+    var sum []int
+    for _, numbers := range numbersToSum {
+        sum = append(sum, Sum(numbers))
+    }
+    return sum
+}
